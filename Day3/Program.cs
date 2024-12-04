40a255308ed3282a0749fafb301da830c59471af
rosco12345 <ross.burnett@gearset.com>
2024-12-04 00:13:54 +0000

    Add day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+var total = 0;
+var enabled = true;
+var input = File.ReadAllText("../../../input.txt");
+
+string pattern = @"mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)";
+
+var matches = Regex.Matches(input, pattern);
+
+foreach (Match match in matches)
+{
+    switch (match.Value)
+    {
+        case "don't()":
+            enabled = false;
+            break;
+        case "do()":
+            enabled = true;
+            break;
+        default:
+            if (enabled)
+            {
+                total += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+            }
+            break; 
+            
+    }
+    
+    
+    
+}
+
+Console.WriteLine(total);
